9ae3943b209435e453e10202d42a11471991971b
Oxygen15-9 <mohammedrahulshaikh@gmail.com>
2024-08-31 18:25:32 +0530

    Signup and Login page
    Used GSAP for animation, currently working to make responsive

--- signup/script.js
+++ signup/script.js
@@ -1,7 +1,61 @@
-let a = document.querySelectorAll('.input')
-let btn = document.querySelector('.button')
-console.log(a);
+let login = document.querySelector('#box1 .link a')
+let overlay = document.querySelector('.overlay')
+let signup = document.querySelector('.signup .link a')
 
-btn.addEventListener('click', function(){
-	btn.innerHTML = 'hii'
+login.addEventListener('click',()=>{
+	gsap.to(overlay,{
+		x:'-90%',
+		duration:1
+	})
+	gsap.to('.box2',{
+		opacity:0,
+		x:-200,
+		duration:1
+	})
+	gsap.to('#box1 form,#box1 h2,#box1 .link',{
+		opacity:0,
+		x:200,
+		duration:1
+	})
+	gsap.to('.signleft',{
+		x:'90%',
+		duration: 1,
+		delay: 1,
+		opacity:1
+	})
+	gsap.to('.signright',{
+		x:'-80%',
+		duration: 1,
+		delay: 1,
+		opacity:1
+	})
+})
+
+signup.addEventListener('click',()=>{
+	gsap.to(overlay,{
+		x:'0%',
+		duration: 1
+	})
+	gsap.to('.box2',{
+		opacity:1,
+		x:0,
+		duration:1,
+		delay: 1
+	})
+	gsap.to('#box1 form,#box1 h2,#box1 .link',{
+		opacity:1,
+		x:0,
+		duration:1,
+		delay: 1
+	})
+	gsap.to('.signleft',{
+		x:0,
+		opacity: 0,
+		duration: 1,
+	})
+	gsap.to('.signright',{
+		x:0,
+		opacity:0,
+		duration: 1,
+	})
 })
